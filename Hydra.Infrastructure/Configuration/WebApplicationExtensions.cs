7c4aa5ac471aa76b03ee3ac18a19bd9df62ad85c
info.aliahmadi <info.aliahmadi@gmail.com>
2024-01-08 13:31:19 +0330

    fix swagger*

--- Hydra.Infrastructure/Configuration/WebApplicationExtensions.cs
+++ Hydra.Infrastructure/Configuration/WebApplicationExtensions.cs
@@ -21,11 +21,11 @@ namespace Hydra.Infrastructure.Configuration
         public static void ConfigureRequestPipeline(this WebApplication app)
         {
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
                 app.UseSwagger();
                 app.UseSwaggerUI();
-            }
+            //}
 
             app.UseHttpsRedirection();
 
