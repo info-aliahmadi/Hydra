d6c26988b2d23d7f95bd62e8e9ee944f632bc4d9
info.aliahmadi <info.aliahmadi@gmail.com>
2024-01-08 13:25:01 +0330

    resolve docker error

--- Hydra.Web/Program.cs
+++ Hydra.Web/Program.cs
@@ -6,40 +6,39 @@ using System.Reflection;
 
 //SerilogStartup.ConfigureLogging();
 
-try
-{
-    var builder = WebApplication.CreateBuilder(args);
+
+var builder = WebApplication.CreateBuilder(args);
 
 
-    builder.Services.ConfigureApplicationServices(builder);
+builder.Services.ConfigureApplicationServices(builder);
 
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-
-
-    // Update Database
-    builder.Services.AddDbContext<MigrationContext>((serviceProvider, options) =>
-        options.UseSqlServer(connectionString), ServiceLifetime.Transient);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 
-    using (ServiceProvider serviceProvider = builder.Services.BuildServiceProvider())
-    {
-        var context = serviceProvider.GetRequiredService<MigrationContext>();
-        context.Database.Migrate();
-    }
+// Update Database
+builder.Services.AddDbContext<MigrationContext>((serviceProvider, options) =>
+    options.UseSqlServer(connectionString), ServiceLifetime.Transient);
+//try
+//{
+
+//    using (ServiceProvider serviceProvider = builder.Services.BuildServiceProvider())
+//    {
+//        var context = serviceProvider.GetRequiredService<MigrationContext>();
+//        context.Database.Migrate();
+//    }
+//}
+//catch (System.Exception ex)
+//{
+//    //Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+//    //throw;
+//}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.ConfigureRequestPipeline();
 
 
-    var app = builder.Build();
+app.Run();
 
-    // Configure the HTTP request pipeline.
-    app.ConfigureRequestPipeline();
-
-
-    app.Run();
-}
-catch (System.Exception ex)
-{
-    //Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
-    //throw;
-}
 public partial class Program { }
