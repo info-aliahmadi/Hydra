4bacd0e4d0109cedd636902f9562607575febd95
info.aliahmadi <info.aliahmadi@gmail.com>
2024-01-08 14:11:40 +0330

    create upload folder*

--- Hydra.Infrastructure/StaticFiles/StaticFilesStartup.cs
+++ Hydra.Infrastructure/StaticFiles/StaticFilesStartup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.FileProviders;
+using System.IO;
 
 namespace Hydra.Infrastructure.StaticFiles
 {
@@ -11,40 +12,81 @@ namespace Hydra.Infrastructure.StaticFiles
         /// <param name="application">Builder for configuring an Files's request pipeline</param>
         public static void UseStaticFiles(this WebApplication app)
         {
+
+            var avatarPath = Path.Combine(app.Environment.ContentRootPath, "images/avatar");
+            if (!Directory.Exists(avatarPath))
+            {
+                Directory.CreateDirectory(avatarPath);
+            }
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(
-               Path.Combine(app.Environment.ContentRootPath, "images/avatar")),
+                avatarPath),
                 RequestPath = "/avatar"
             });
+
+
+            var imagesPath = Path.Combine(app.Environment.ContentRootPath, "uploads/images");
+            if (!Directory.Exists(imagesPath))
+            {
+                Directory.CreateDirectory(imagesPath);
+            }
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(
-               Path.Combine(app.Environment.ContentRootPath, "uploads/images")),
+               imagesPath),
                 RequestPath = "/images"
             });
+
+
+            var videosPath = Path.Combine(app.Environment.ContentRootPath, "uploads/videos");
+            if (!Directory.Exists(videosPath))
+            {
+                Directory.CreateDirectory(videosPath);
+            }
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(
-               Path.Combine(app.Environment.ContentRootPath, "uploads/videos")),
+               videosPath),
                 RequestPath = "/videos"
             });
+
+
+            var audioPath = Path.Combine(app.Environment.ContentRootPath, "uploads/audio");
+            if (!Directory.Exists(audioPath))
+            {
+                Directory.CreateDirectory(audioPath);
+            }
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(
-               Path.Combine(app.Environment.ContentRootPath, "uploads/audio")),
+               audioPath),
                 RequestPath = "/audio"
             });
+
+
+            var documentsPath = Path.Combine(app.Environment.ContentRootPath, "uploads/documents");
+            if (!Directory.Exists(documentsPath))
+            {
+                Directory.CreateDirectory(documentsPath);
+            }
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(
-               Path.Combine(app.Environment.ContentRootPath, "uploads/documents")),
+               documentsPath),
                 RequestPath = "/documents"
             });
+
+
+            var othersPath = Path.Combine(app.Environment.ContentRootPath, "uploads/others");
+            if (!Directory.Exists(othersPath))
+            {
+                Directory.CreateDirectory(othersPath);
+            }
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(
-               Path.Combine(app.Environment.ContentRootPath, "uploads/others")),
+               othersPath),
                 RequestPath = "/others"
             });
 
