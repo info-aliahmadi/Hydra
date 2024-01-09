c7853e08c10dfc668956752d2a3e963faa9298b2
info.aliahmadi <info.aliahmadi@gmail.com>
2024-01-09 14:26:04 +0330

    mig

--- Hydra.Web/Program.cs
+++ Hydra.Web/Program.cs
@@ -12,26 +12,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureApplicationServices(builder);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 
-// Update Database
-builder.Services.AddDbContext<MigrationContext>((serviceProvider, options) =>
-    options.UseSqlServer(connectionString), ServiceLifetime.Transient);
-try
-{
+//// Update Database
+//builder.Services.AddDbContext<MigrationContext>((serviceProvider, options) =>
+//    options.UseSqlServer(connectionString), ServiceLifetime.Transient);
+//try
+//{
 
-    using (ServiceProvider serviceProvider = builder.Services.BuildServiceProvider())
-    {
-        var context = serviceProvider.GetRequiredService<MigrationContext>();
-        context.Database.Migrate();
-    }
-}
-catch (System.Exception ex)
-{
-    //Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
-    //throw;
-}
+//    using (ServiceProvider serviceProvider = builder.Services.BuildServiceProvider())
+//    {
+//        var context = serviceProvider.GetRequiredService<MigrationContext>();
+//        context.Database.Migrate();
+//    }
+//}
+//catch (System.Exception ex)
+//{
+//    //Log.Fatal($"Failed to start {Assembly.GetExecutingAssembly().GetName().Name}", ex);
+//    //throw;
+//}
 
 var app = builder.Build();
 
