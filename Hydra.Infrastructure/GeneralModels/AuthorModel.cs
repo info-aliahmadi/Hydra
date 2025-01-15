5ed6d8cccba5178f972e656d7eeeacf35020c761
info.aliahmadi <info.aliahmadi@gmail.com>
2025-01-15 16:44:38 +0330

    Add Author model

---
+++ Hydra.Infrastructure/GeneralModels/AuthorModel.cs
@@ -0,0 +1,17 @@
+﻿namespace Hydra.Infrastructure.GeneralModels
+{
+    public record AuthorModel
+    {
+        public int? Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string? Avatar { get; set; }
+
+
+    }
+}
